aa3bd7d85a4e75f0517257cc54edb195e0184b30
Jaime Salas <35207246+Josalas16@users.noreply.github.com>
2019-08-31 18:37:05 -0600

    Add files via upload

--- wordCount.py
+++ wordCount.py
@@ -1,6 +1,42 @@
 # CS 4375: Theory of Operating Systems
 # Author: Jaime O. Salas
 # Description:
-import re
+
+# Helps match or finds strings (Regular Expression)
+import re 
+# Interaction with the file system
 import os
+# Constants functions and methods
 import sys
+
+
+# Input and output Files
+fileInput = sys.argv[1]
+fileOutput= sys.argv[2]
+
+# Dictionary for all words
+wordfileDict = {}
+
+# Opening a file
+with open(fileInput, "r") as wordFile:
+    for line in wordFile:
+        #removes newlines
+        line = line.strip()
+        
+        line = line.lower()
+        
+        words = line.split(' ')
+        
+        for word in words:
+            if word in wordfileDict:
+                wordfileDict[word] += 1
+            else:
+                wordfileDict[word] = 1
+                
+with open(fileOutput, 'w') as f:
+    for key in list(wordfileDict.keys()):
+        f.write((key, ':', str(wordfileDict[key])))
+    
+
+                
+        
